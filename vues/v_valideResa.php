3c4b0cd43f1f042da89a3db067f18d3585df5c9a
etudiant <etudiant@10.20.162.113>
2017-10-05 18:06:33 +0200

    05/10/17

--- vues/v_valideResa.php
+++ vues/v_valideResa.php
@@ -1,17 +1,64 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-       
-        ?>
-    </body>
-</html>
+<?php
+$nomclient = $REQUEST["nomClient"];
+    $prenomclient = $REQUEST["prenomClient"];
+    $numeroVol = $REQUEST["numeroVolt"];
+    $dateDepart= $REQUEST["dateDepart"];
+    $heureDepart= $REQUEST["heureDepart"];
+    $dateArrivee = $REQUEST["dateArrivee"];
+    $heureArrivee=$REQUEST["heureArrivee"];
+    $prix=$REQUEST["prix"];
+    $depart=$REQUEST["depart"];
+    $arrivee=$REQUEST["arrivee"];
+
+?>
+<input type="submit" value="Valider" />
+<form action="index?action=voirResa" methode="POST">
+  
+<?php
+ echo"     <tr>
+              <th>
+                Numéro du Vol :
+                </th>
+                
+                <td>
+                $numeroVol
+                </td>
+                <th>
+                Nom :
+              </th>
+            </tr>
+                
+              <tr>
+                <td>
+                $nomClient
+                </td>
+                <th>
+                Prénom :
+                </th>
+                <td>
+                $prenomClient
+                </td>
+                </th>
+                </tr>
+                
+                <th>
+                Date et heure de départ :
+                </th>
+                <td>
+                $dateDepart
+                $heureDepart
+                </td>
+                </th>
+                </tr>
+                
+                <th>
+                Date et heure d'arriver :
+                </th>
+                <td>
+                $dateArrivee
+                $heureArrivee 
+                </td>
+                </tr>
+  "
+?> 
+</form>

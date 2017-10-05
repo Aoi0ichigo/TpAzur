093b1bf63d299048efa2f86c975bade7740918bd
etudiant <etudiant@10.20.162.113>
2017-10-05 18:05:57 +0200

    05/10/17

--- modele/fonctions.php
+++ modele/fonctions.php
@@ -1,131 +1,129 @@
-<?php
-    function getLesVols()
-    {
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="ISO-8859-1">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            function getLesVols()
+            {
 
-    // D�claration d'un tableau
+            // D�claration d'un tableau
 
-    $vols = array();
+            $vols = array();
 
-      // Appel au fichier permettant la connection � la BD
-     require dirname(__FILE__)."/Connection.php";
-     // Selection de la base de donn�es et requete SQL
-     $requete="select numero,A1.nomAero as depart,dateDepart,heureDepart,A2.nomAero as arrive,dateArrivee,heureArrivee,prix from vol "
-             . "join aeroport as A1 on A1.numAero=depart "
-             . "join aeroport as A2 on A2.numAero=arrivee";
-    // Remplissage d'un tableau correspondant � chaque vol
-        $bdd= connect();
-        $i=0;
-        try 
-        {	
-            $sql = $bdd->prepare($requete);
-            $sql->execute();
+              // Appel au fichier permettant la connection � la BD
+             require dirname(__FILE__)."/Connection.php";
+             // Selection de la base de donn�es et requete SQL
+             $requete="select numero,A1.nomAero as depart,dateDepart,heureDepart,A2.nomAero as arrive,dateArrivee,heureArrivee,prix from vol "
+                     . "join aeroport as A1 on A1.numAero=depart "
+                     . "join aeroport as A2 on A2.numAero=arrivee";
+            // Remplissage d'un tableau correspondant � chaque vol
+                $bdd= connect();
+                $i=0;
+                try 
+                {	
+                    $sql = $bdd->prepare($requete);
+                    $sql->execute();
+                    
+                    while($ligne=$sql->fetch(PDO::FETCH_OBJ))
+                    { 
+                        
+                        $unVol[$i]= ["numero"=>$ligne->numero,
+                            "depart"=>$ligne->depart,
+                            "dateDepart"=>$ligne->dateDepart,
+                            "heureDepart"=>$ligne->heureDepart,
+                            "arrivee"=>$ligne->arrive,
+                            "dateArrivee"=>$ligne->dateArrivee,
+                            "heureArrivee"=>$ligne->heureArrivee,
+                            "prix"=>$ligne->prix];
+                        $i++;
+                    } 
+                    
+                }
+                catch(PDOException $e)
+                {
+                    echo "Erreur dans la requ�te" . $e->getMessage();
+                }
 
-            while($ligne=$sql->fetch(PDO::FETCH_OBJ))
-            { 
+             // Remplissage du tableau multi-dimensionnel $vols avec chacun des vols
 
-                $unVol[$i]= ["numero"=>$ligne->numero,
-                    "depart"=>$ligne->depart,
-                    "dateDepart"=>$ligne->dateDepart,
-                    "heureDepart"=>$ligne->heureDepart,
-                    "arrivee"=>$ligne->arrive,
-                    "dateArrivee"=>$ligne->dateArrivee,
-                    "heureArrivee"=>$ligne->heureArrivee,
-                    "prix"=>$ligne->prix];
-                $i++;
-            } 
+                for($r=0;$r<$i;$r++){
+                    array_push($vols,$unVol[$r]);
+                }
 
-        }
-        catch(PDOException $e)
-        {
-            echo "Erreur dans la requ�te" . $e->getMessage();
-        }
+            // Retourner le tableau
 
-     // Remplissage du tableau multi-dimensionnel $vols avec chacun des vols
+            return $vols;
 
-        for($r=0;$r<$i;$r++){
-            array_push($vols,$unVol[$r]);
-        }
+            }
+            
+            function getLesResa(){
+               
+            // D�claration d'un tableau
 
-    // Retourner le tableau
+            $reservations = array();
 
-    return $vols;
+              // Appel au fichier permettant la connection � la BD
+             require dirname(__FILE__)."/Connection.php";
+             // Selection de la base de donn�es et requete SQL
+                $requete="select * from reservation ";/*where numAgence=:numAgence*/
+            // Remplissage d'un tableau correspondant � chaque reservation
+                $bdd= connect();
+                $i=0;
+                try 
+                {	
+                    $sql = $bdd->prepare($requete);
+                    //$sql->bindParam( ':numAgence',$SESSION['agence'],PDO::PARAM_STR);
+                    $sql->execute();
+                    
+                    while($ligne=$sql->fetch(PDO::FETCH_OBJ))
+                    { 
+                        
+                        $uneResa[$i]= [
+                            "nomClient"=>$ligne->nomClient,
+                            "prenomClient"=>$ligne->prenomClient,
+                            "numero"=>$ligne->numVol,
+                            "qdPlace"=>$ligne->nbPlace
+                        ];
+                        $i++;
+                    } 
+                    
+                }
+                catch(PDOException $e)
+                {
+                    echo "Erreur dans la requ�te" . $e->getMessage();
+                }
 
-    }
+             // Remplissage du tableau multi-dimensionnel $vols avec chacun des vols
 
-    function getLesResa(){
+                for($r=0;$r<$i;$r++){
+                    array_push($reservations,$uneResa[$r]);
+                }
 
-    // D�claration d'un tableau
+            // Retourner le tableau
 
-    $reservations = array();
+            return $reservations;
+            
+             }
+            function reserverVol(){
+                // récup numéro vol
+                $numero = $_REQUEST["numero"];
+                return $numero; 
+            }
 
-      // Appel au fichier permettant la connection � la BD
-     require dirname(__FILE__)."/Connection.php";
-     // Selection de la base de donn�es et requete SQL
-        $requete="select * from reservation ";
-    // Remplissage d'un tableau correspondant � chaque reservation
-        $bdd= connect();
-        $i=0;
-        try 
-        {	
-            $sql = $bdd->prepare($requete);
-            $sql->execute();
-
-            while($ligne=$sql->fetch(PDO::FETCH_OBJ))
-            { 
-
-                $uneResa[$i]= [
-                    "nomClient"=>$ligne->nomClient,
-                    "prenomClient"=>$ligne->prenomClient,
-                    "numero"=>$ligne->numVol,
-                    "qdPlace"=>$ligne->nbPlace
-                ];
-                $i++;
-            } 
-
-        }
-        catch(PDOException $e)
-        {
-            echo "Erreur dans la requ�te" . $e->getMessage();
-        }
-
-     // Remplissage du tableau multi-dimensionnel $vols avec chacun des vols
-
-        for($r=0;$r<$i;$r++){
-            array_push($reservations,$uneResa[$r]);
-        }
-
-    // Retourner le tableau
-
-    return $reservations;
-
-     }
-    function reserverVol(){
-        // récup numéro vol
-        $numero = $_REQUEST["numero"];
-        return $numero; 
-    }
-
-    function valideResa(){
-        $numVol=$_POST['numero'];
-        $nom=$_POST['nom'];
-        $prenom=$_POST['prenom'];
-        $mail=$_POST['mail'];
-        $adresse=$_POST['adresse'];
-        $nbPlace=$_POST['nbPlace'];
-         // Appel au fichier permettant la connection � la BD
-     require dirname(__FILE__)."/Connection.php";
-     // Selection de la base de donn�es et requete SQL
-        $requete="insert into reservation values (null,'$nom','$prenom','$adresse','$mail','$nbPlace','$numVol') ";
-        $bdd= connect();   
-        try 
-        {	
-            $sql = $bdd->prepare($requete);
-            $sql->execute();
-        }
-        catch(PDOException $e)
-        {
-            echo "Erreur dans la requ�te" . $e->getMessage();
-        }
-    }
-
- ?>
+           
+            
+            function formulaireReservation(){
+                ;
+            }
+         ?>
+        
+    </body>
+</html>
